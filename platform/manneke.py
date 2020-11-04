7e37f0d32030e2349e74d6b1713e48936cce45f1
Robin De Greve <rdegreve@delhaize.be>
2020-11-04 18:28:27 +0100

    hitbox bijna af

--- platform/manneke.py
+++ platform/manneke.py
@@ -4,16 +4,23 @@ from time import sleep
 
 class Manneke(turtle.Turtle):
 
+    size_w = 3
+    size_l = 3
+    act_w = size_w * 10
+    act_l = size_l * 10
+    jump_h = 250
+    tilted = "right"
+
     def __init__(self):
         super(Manneke, self).__init__()
         self.speed(0)
         self.shape("turtle")
-        self.color("brown")
+        self.color("green")
         self.penup()
         self.goto(-350, -157)
-        self.shapesize(stretch_len=3, stretch_wid=3)
+        self.shapesize(stretch_len=self.size_l, stretch_wid=self.size_w)  # maal 15
         self.dx = 0.1
-        self.dy = 0.1
+        self.dy = 0.07
 
 
     def go_left(self):
@@ -21,16 +28,24 @@ class Manneke(turtle.Turtle):
         x -= 20
         self.setx(x)
 
+        if self.tilted == "right":
+            self.tilt(180)
+            self.tilted = "left"
+
 
     def go_right(self):
         x = self.xcor()
         x += 20
         self.setx(x)
 
+        if self.tilted == "left":
+            self.tilt(180)
+            self.tilted = "right"
+
 
     def jump(self):
         if self.ycor() <= -157:
-            self.sety(self.ycor() + 100)
+            self.sety(self.ycor() + self.jump_h)
 
 
     def gravity(self):
--- platform/obstakel.py
+++ platform/obstakel.py
@@ -3,23 +3,47 @@ import turtle
 
 class Obstakel(turtle.Turtle):
 
-    ycor = -157
+    ypos = -157
+    size_w = 3
+    size_l = 1
+    act_w = size_w * 10
+    act_l = size_l * 10
 
     def __init__(self, xpos):
         super(Obstakel, self).__init__()
         self.penup()
-        self.goto(xpos, self.ycor)
+        self.goto(xpos, self.ypos)
         self.shape("square")
         self.resizemode("user")
-        self.shapesize(stretch_len=1, stretch_wid=3)
-        self.color("green")
+        self.shapesize(stretch_len=self.size_l, stretch_wid=self.size_w)  # maal 15
+        self.color("#663300")
         self.xpos = xpos
 
 
     def check_colision(self, player):
-        # turtle mag niet tussen x coords zitten
-        # -157 is midden, dus outline is -147 en -167
-        if (player.xcor() > (self.xpos - 60)) and (player.ycor() < self.ycor):
-            player.setx(self.xpos - 60)
+        # komende van links
+        # check eerst x-as
+        # dan y-as
+        # alleen terwijl in linker deel
+        if ((player.xcor() + player.act_w) > (self.xpos - self.act_w)) \
+                and ((player.ycor() - player.act_l) < (self.ypos + self.act_l)) \
+                and (player.xcor() < self.xpos):
+            player.setx(player.xcor() - player.size_w)
 
+        # komende van rechts
+        if ((player.xcor() - player.act_w) < (self.xpos + self.act_w)) \
+                and ((player.ycor() - player.act_l) < (self.ypos + self.act_l)) \
+                and (player.xcor() > self.xpos):
+            player.setx(player.xcor() + player.size_w)
+
+
+    # hitbox
+    #   x-as links:
+    #       self.ycor - (self.size_w * 10) - (player.size_w * 10)
+    #
+    #   x-as rechts:
+    #       self.ycor + (self.size_w * 10) + (player.size_w * 10)
+    #
+    #   y-as boven:
+    #
 
